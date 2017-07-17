3bb128e570710ef367697f239527d05f5048ec1b
Elexy <alex@seretalabs.com>
2017-07-17 18:11:05 +0200

    adds all necesary  parameters for GCE authentication

--- scripts/init_gce.sh
+++ scripts/init_gce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
 usage() {
 cat << EOF
@@ -8,33 +8,54 @@ cat << EOF
 Usage: ${0##*/} [-g [base64 encoded gcloud-key.json]] [-h]
 
 Setup gCloud SDK and configure access.
-Requires a gCloud service from https://console.cloud.google.com/iam-admin/serviceaccounts
+Requires a gCloud service account from https://console.cloud.google.com/iam-admin/serviceaccounts
+these environment variables can optionally be set:
+GCLOUD_PROJECT GCLOUD_SERVICE_ACCOUNT GCE_CLUSTER GCE_ZONE
 
     Instructions:
     1. Select an account with the required priviledges and create a new key from the context menu.
     2. This will download a file, store that in a safe place.
     3. Encode the entire file with base64 and either export an ENV var:
-        "export GCLOUD_SERVICE_KEY=`cat ~/gcloud.json | base64`"
+        "export GCLOUD_SERVICE_KEY=\`cat ~/gcloud.json | base64\`"
       or supply on the command line:
-        "${0##*/} -g `cat ~/gcloud.json | base64`"
+        "${0##*/} -g \`cat ~/gcloud.json | base64\`"
 
 
     -h              display this help and exit
-    -g              gCloud service key
+    -k              gCloud service key
+    -p              gCloud project name
+    -s              gCloud service account
+    -c              gCloud cluster name
+    -z              gCloud zone
 
 EOF
 }
 
+CLOUDSDK_VERSION='162.0.0'
+GSK=$GCLOUD_SERVICE_KEY
+
 # getopts & validations
 OPTIND=1 # Reset is necessary if getopts was used previously in the script.  It is a good idea to make this local in a function.
-while getopts "hg:" opt; do
+while getopts "hk:p:s:c:z:" opt; do
   case "$opt" in
     h)
         usage
         exit 0
         ;;
-    g)
-        GCLOUD_SERVICE_KEY=${OPTARG}
+    k)
+        GSK=${OPTARG}
+        ;;
+    p)
+        GCLOUD_PROJECT=${OPTARG}
+        ;;
+    s)
+        GCLOUD_SERVICE_ACCOUNT=${OPTARG}
+        ;;
+    c)
+        GCE_CLUSTER=${OPTARG}
+        ;;
+    z)
+        GCE_ZONE=${OPTARG}
         ;;
     \?)
         usage >&2
@@ -44,7 +65,7 @@ while getopts "hg:" opt; do
 done
 shift "$((OPTIND-1))" # Shift off the options and optional --.
 
-if [ -z "$GCLOUD_SERVICE_KEY" ]; then
+if [ -z "$GSK" ]; then
   echo error: no [GCLOUD_SERVICE_KEY] supplied of found in the ENV
   usage
   exit 1
@@ -57,12 +78,32 @@ OLDDIR=$PWD
 cd $WORKDIR
 echo "working directory: $PWD"
 
-echo "installing gCloud SDK"
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-156.0.0-linux-x86_64.tar.gz
-tar -zxf google-cloud-sdk-156.0.0-linux-x86_64.tar.gz
+if hash gdate 2>/dev/null; then
+    echo "using existing gcloud command"
+else
+    echo "installing gCloud SDK"
+    wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$CLOUDSDK_VERSION-linux-x86_64.tar.gz
+    tar -zxf google-cloud-sdk-$CLOUDSDK_VERSION-linux-x86_64.tar.gz
+fi
+
+declare -a vars=(GCLOUD_PROJECT GCLOUD_SERVICE_ACCOUNT GCE_CLUSTER GCE_ZONE)
+
+for var_name in "${vars[@]}"
+do
+  if [ -z "${!var_name}" ]; then
+    echo "Missing option or environment variable or config key $var_name"
+    usage >&2
+    exit 1
+  fi
+done
 
 echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
-chmod -R 777 ./google-cloud-sdk
-export PATH=${HOME}/google-cloud-sdk/bin:$PATH
+gcloud config set --installation component_manager/disable_update_check true
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+export GOOGLE_APPLICATION_CREDENTIALS=${HOME}/gcloud-service-key.json
+export CLOUDSDK_CONTAINER_USE_CLIENT_CERTIFICATE=True
+gcloud config set project $GCLOUD_PROJECT
+gcloud config set account $GCLOUD_SERVICE_ACCOUNT
+gcloud container clusters get-credentials $GCE_CLUSTER --zone $GCE_ZONE --project $GCLOUD_PROJECT
 
 cd $OLDDIR
